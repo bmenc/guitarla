4cc8b8a121e0fc945ba14f347e07eeb5b2d10845
bmenc <bmenchaca.cca@gmail.com>
2025-08-27 20:33:22 -0600

    64. Iterando sobre los Productos en React

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,20 @@
 import { useEffect, useState } from 'react';
 import Guitar from './components/Guitar';
 import Header from './components/Header';
+import { db } from './data/db';
 
 function App() {
-
-  const [auth, setAuth] = useState(false);
-
-  useEffect(() => { 
-    if (auth) {
-      console.log('Componente Listo o escuchando por Auth');
-    }
-  }, [auth])
-
-  setTimeout(() => {
-    setAuth(true)
-  }, 3000);
-
+  const [data, setData] = useState(db);
+  
   return (
     <>
-    <Header/>
-
+      <Header />
       <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
-
         <div className="row mt-5">
-          <Guitar />
-          <Guitar />
-          <Guitar />
-          <Guitar />
-          <Guitar />  
-          <Guitar />
+          {data.map(() => (<Guitar/>))}
         </div>
       </main>
-
       <footer className="bg-dark mt-5 py-5">
         <div className="container-xl">
           <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
