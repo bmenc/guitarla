c71f842eedeecc204702bbdd5b25eed122e2cf74
bmenc <bmenchaca.cca@gmail.com>
2025-08-31 14:24:46 -0600

    71. Agregando Guitarras al Carrito con Cantidad

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,12 @@ function App() {
 
   function addToCart(item) {
     const itemExist = cart.findIndex(guitar => guitar.id === item.id);
-    if (itemExist)
-    console.log(itemExist);
-    setCart(prevCart => [...prevCart, item])
+    if (itemExist >= 0) { //Existe en el carrito
+      console.log("ya existe");
+    } else {
+      item.quantity = 1
+      setCart(prevCart => [...prevCart, item]);
+    }
   }
   
   return (
