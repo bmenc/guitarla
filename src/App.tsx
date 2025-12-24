8033e9e3c72795f628410841c8120f8ff5152b47
bmenc <bmenchaca.cca@gmail.com>
2025-12-23 21:18:06 -0600

    171. Instanciando el reducer en la página principal

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
+import { useReducer } from 'react';
 import Guitar from './components/Guitar';
 import Header from './components/Header';
 import { useCart } from './hooks/useCart';
+import { cartReducer, initialState } from './reducers/cart-reducer';
 
 function App() {
 
@@ -14,6 +16,9 @@ function App() {
           isEmpty,
           cartTotal
         } = useCart();
+
+    const [state, dispatch] = useReducer(cartReducer, initialState);
+console.log(state);
 
   return (
     <>
